28c1bcde9c88d1f48d56257ac5ce988ad8ff0dee
jlbernard <bernard.logan4@gmail.com>
2014-06-23 16:53:18 -0400

    all desired data as JSON

--- allrecipes.js
+++ allrecipes.js
@@ -51,29 +51,40 @@ function getInstructions($){
 //  id 2,3 are the TOTAL time
 function getCookTime($){
   var json = {id: {}}
-  $('div[id="divRecipeTimesContainer"]').find('span').each(function(i){
+  $('div#divRecipeTimesContainer').find('span').each(function(i){
     json.id[i] = $(this).text()
   })
   return JSON.stringify(json)
 }
 
 
-//
-// IN PROGRESS
-//
-function getPosReview($){
-  console.log('-------')
-  console.log('Most Helpful Positive Review: ')
-  var rating = $('div')
-  $(times).find('span').each(function(i){
-    var labels = ['Prep: ','Cook: ','Total: ', 'Total: ']
-    console.log(labels[i] + $(this).text())
+// returns the best positive/critical review
+// as id 0 and 1
+function getReviews($){
+  var json = {id: {}}
+  $('div[itemprop="review"]').find('p').each(function(i){
+    if(i==2)
+      return false
+    json.id[i] = $(this).text()
   })
+  return JSON.stringify(json)
+}
+
+
+// each id has a category and its quantity
+// ie 0: Calories 175g
+function getNutrition($){
+  var json = {id: {}}
+  $('ul#ulNutrient').each(function(i){
+    data = $(this).find('li').text()
+    // remove whitespace after type/quantity
+    json.id[i] = data.slice(0, data.indexOf("  "))
+  })
+  return JSON.stringify(json)
 }
 
 
 // parse recipe provided, return JSON of all functions
-//
 function recipeInfo(recipe) {
   console.log('*********************************')
   console.log('Recipe Selected: ' + recipe)
@@ -82,18 +93,19 @@ function recipeInfo(recipe) {
   request(url, function(err, resp, body) {
    // create a DOM of the provided response body, call other scrapers
     $ = cheerio.load(body);
-
     var json = {
       rating: getRating($),
       time: getCookTime($),
       ingredients: getIngredients($),
-      instructions: getInstructions($)
+      instructions: getInstructions($),
+      review: getReviews($),
+      nutrition: getNutrition($)
     }
-    
+
     var stringed = JSON.stringify(json, null, 4)
     console.log(JSON.parse(stringed))
     fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){})
   })
 }
 
-recipeInfo('chicken-pot-pie-ix')
+recipeInfo('garlic-bread')
